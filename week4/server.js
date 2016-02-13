837fc57087f5cdde2448af0f587d0ece87ff0eae
Victor Aguwa <victoraguwa94@gmail.com>
2016-02-12 20:29:08 -0500

    node update

--- week4/server.js
+++ week4/server.js
@@ -2,8 +2,8 @@ const http = require('http');
 
 http.createServer( (request, response) => {
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('Hello World\n');
-  response.end('This is a node app!'\n');
+  response.write('<h1>Hello World</h1>\n');
+  response.end('<p>This is a node app!<p>\n');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
